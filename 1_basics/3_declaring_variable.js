8e23aa88f8c9a0d4be22002d6c90c61fed6fc095
juyeon <juyun7908@naver.com>
2023-10-17 17:14:29 +0900

    first commit

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,45 @@
+/**
+ * variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let
+ * 3) const
+ */
+
+var name = '코드팩토리';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '코드팩토리'; -> 값 변경 불가능
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것.
+ * 2) 할당
+ */
+let girlFriend;
+console.log(girlFriend);
+
+/**
+ * var과 let으로 변수를 선언하면
+ * 자동으로 undefined로 할당이 됨
+ */
+
+// const girlFriend2; -> 불가능!
